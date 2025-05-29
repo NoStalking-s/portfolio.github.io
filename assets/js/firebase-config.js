6447c73e877bbff898d275eca0e43166a3c3661f
NoStalking <118044874+NoStalking-s@users.noreply.github.com>
2025-05-29 20:55:04 +0500

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,19 @@
+// ЗАМЕНИ значения на свои из консоли Firebase!
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBS-MxYSCoMeKcYrJoTvVlcBd8l08gO_Ms",
+  authDomain: "kamolovportfolio.firebaseapp.com",
+  projectId: "kamolovportfolio",
+  storageBucket: "kamolovportfolio.firebasestorage.app",
+  messagingSenderId: "184302069372",
+  appId: "1:184302069372:web:84bf8d6257f0af740f0bf9",
+  measurementId: "G-YNR1KQJ5C2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const analytics = getAnalytics(app);
+
+export { db, ref, push };
